b153cac1bf6aa9820ad3d74fb780632636e50ef8
qiangzi2018 <978654206@qq.com>
2018-10-03 15:35:43 +0800

    2018.10.03 第一次git提交

---
+++ git_test/src/test/java/com/itheima/GitTest.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+import org.junit.Test;
+
+/**
+ * @author zhongqiang
+ * @2018/10/3 15:24
+ */
+public class GitTest {
+    @Test
+    public void test01(){
+        int i = 1;
+        System.out.println("hello git project"+i++);
+        System.out.println("hello git project"+i++);
+        System.out.println("hello git project"+i++);
+        System.out.println("hello git project"+i++);
+        System.out.println("hello git project"+i++);
+        System.out.println("hello git project"+i++);
+    }
+}
